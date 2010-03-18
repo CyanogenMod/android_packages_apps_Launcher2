1297e31a2414d5f8181bc1a50679b2eadf6337dd
kmobs <persiansown@gmail.com>
2010-03-18 03:34:13 -0500

    Changed appdraw to have 5 columns.  adding all the icons comes next
    5 icons are now displayed per row.  Needs tweaking
    
    Change defined number of columns to 5
    
    Row height now correctly calculated, removing the 'black bar'
    
    Fixed the icon shifting present before by adjusting row offset
    
    Adds 5 icons to top and bottom animated rows.  Still need to be shifted over though.
    
    Didn't mean to push this change.
    
    Turns out we DID need this.  Its what causes the proper icon to appear on the bottom row's animation
    
    Fixes the funky animations on the top and bottom

--- res/raw/rollo3.c
+++ res/raw/rollo3.c
@@ -253,7 +253,7 @@ void updatePos() {
 int positionStrip(float row, float column, int isTop, float p, int isText)
 {
     float mat1[16];
-    float x = 0.5f * (column - 1.5f);
+    float x = 0.5f * 4/5 * (column - 2.f) ;
     float scale = 72.f * 3 / getWidth();
 
     if (isTop) {
@@ -304,14 +304,14 @@ void drawFrontGrid(float rowOffset, float p)
 
     int intRowOffset = rowOffset;
     float rowFrac = rowOffset - intRowOffset;
-    float colWidth = getWidth() / 4;
-    float rowHeight = colWidth + 25.f;
+    float colWidth = getWidth() / 5;
+    float rowHeight = (colWidth * 5/4) + 25.f;
     float yoff = h - ((h - (rowHeight * 4.f)) / 2);
 
     yoff -= 110;
 
     int row, col;
-    int iconNum = intRowOffset * 4;
+    int iconNum = intRowOffset * 5;
     float ymax = yoff + rowHeight;
     float ymin = yoff - (3 * rowHeight) - 70;
     float gridTop = yoff -3;
@@ -322,7 +322,7 @@ void drawFrontGrid(float rowOffset, float p)
     for (row = 0; row < 5; row++) {
         float y = yoff - ((-rowFrac + row) * rowHeight);
 
-        for (col=0; col < 4; col++) {
+        for (col=0; col < 5; col++) {
             if (iconNum >= state->iconCount) {
                 return;
             }
@@ -391,7 +391,7 @@ void drawTop(float rowOffset, float p)
     int row, col;
     int iconNum = 0;
     for (row = 0; row <= (int)(rowOffset+1); row++) {
-        for (col=0; col < 4; col++) {
+        for (col=0; col < 5; col++) {
             if (iconNum >= state->iconCount) {
                 return;
             }
@@ -408,9 +408,9 @@ void drawBottom(float rowOffset, float p)
     pos -= rowOffset - intRowOffset;
 
     int row, col;
-    int iconNum = (intRowOffset + 3) * 4;
+    int iconNum = (intRowOffset + 3) * 5;
     while (1) {
-        for (col=0; col < 4; col++) {
+        for (col=0; col < 5; col++) {
             if (iconNum >= state->iconCount) {
                 return;
             }
