b8a251d26d153c2c95fab12276f59f045c94b435
Danesh M <daneshm90@gmail.com>
2011-12-05 00:17:11 -0500

    Launcher : Num. of homescreens + rotation toggle
    Allow user to choose number of homescreens (1-7)
    Allow user to control orientation
    
    Change-Id: I0f62834ad4def588fc6bc9e8937c33afff786afb

--- src/com/android/launcher2/preference/Preferences.java
+++ src/com/android/launcher2/preference/Preferences.java
@@ -19,23 +19,45 @@ package com.android.launcher2.preference;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceScreen;
 
 import com.android.launcher.R;
 
-public class Preferences extends PreferenceActivity {
+public class Preferences extends PreferenceActivity implements OnPreferenceChangeListener {
 
     private static final String TAG = "Launcher.Preferences";
-
+    private static final String HOMESCREEN_COUNT = "ui_homescreen_general_count";
+    private ListPreference mHomescreenCount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
-
+        PreferenceScreen prefSet = getPreferenceScreen();
+        mHomescreenCount = (ListPreference) prefSet.findPreference(HOMESCREEN_COUNT);
+        mHomescreenCount.setOnPreferenceChangeListener(this);
+        setPrefSummary(mHomescreenCount,getResources().getString(R.string.preferences_interface_homescreen_general_count_summary) + mHomescreenCount.getEntry());
         SharedPreferences prefs =
             getSharedPreferences(PreferencesProvider.PREFERENCES_KEY, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = prefs.edit();
                 editor.putBoolean(PreferencesProvider.PREFERENCES_CHANGED, true);
                 editor.commit();
+    }
+
+    private void setPrefSummary(Preference preference, String summary) {
+        preference.setSummary(summary);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if ( preference == mHomescreenCount) {
+            setPrefSummary(mHomescreenCount,getResources().getString(
+                    R.string.preferences_interface_homescreen_general_count_summary) + newValue.toString());
+            return true;
+        }
+        return false;
     }
 }
